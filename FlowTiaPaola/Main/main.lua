ed7b33d1150fd6ac1d77f7bf636dcedc8d01ba4a
BlackDisciplesHook <ilovefarts3123123@gmail.com>
2025-11-20 23:38:04 +0100

    Add files via upload

---
+++ FlowTiaPaola/Main/main.lua
@@ -0,0 +1,346 @@
+for _, thread in getreg() do
+    if typeof(thread) ~= "thread" then
+        continue
+    end
+
+    local Source = debug.info(thread, 1, "s")
+    if Source and (Source:match(".Core.Anti") or Source:match(".Plugins.Anti_Cheat")) then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/cutelilfemboy12/Resources/refs/heads/main/Bypass.lua"))()
+        break
+    end
+end
+
+getgenv().Whitelist = {}
+local Whitelist = getgenv().Whitelist
+
+local ESP = loadstring(game:HttpGet('https://raw.githubusercontent.com/BlackDisciplesHook/FlowTiaPaola/refs/heads/main/FlowTiaPaola/Resources/esp.lua'))()
+local SilentAim = loadstring(game:HttpGet('https://raw.githubusercontent.com/BlackDisciplesHook/FlowTiaPaola/refs/heads/main/FlowTiaPaola/Resources/silentaim.lua'))()
+
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/BlackDisciplesHook/FlowTiaPaola/refs/heads/main/FlowTiaPaola/Library/Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet('https://raw.githubusercontent.com/BlackDisciplesHook/FlowTiaPaola/refs/heads/main/FlowTiaPaola/Library/addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet('https://raw.githubusercontent.com/BlackDisciplesHook/FlowTiaPaola/refs/heads/main/FlowTiaPaola/Library/addons/SaveManager.lua'))()
+
+local Window = Library:CreateWindow({
+    Title = 'FlowTiaPaola | ' .. os.date("%d %b %Y"),
+    Center = true,
+    AutoShow = true,
+    TabPadding = 8,
+    MenuFadeTime = 0.1
+})
+
+local Tabs = {
+    Main = Window:AddTab('Main'),
+    Visuals = Window:AddTab('Visuals'),
+    Settings = Window:AddTab('Settings'),
+}
+
+local SilentAimMainSection = Tabs.Main:AddLeftGroupbox('SilentAim')
+
+SilentAimMainSection:AddToggle('SilentAimEnabled', {
+    Text = "Enabled",
+    Default = false,
+    Callback = function(Value)
+        SilentAim.Config.Enabled = Value
+    end
+})
+
+SilentAimMainSection:AddToggle('SilentAimWallbangEnabled', {
+    Text = "Wallbang",
+    Default = false,
+    Callback = function(Value)
+        SilentAim.Config.BulletTeleport = Value
+    end
+})
+
+SilentAimMainSection:AddSlider('SilentAimSmoothness', {
+    Text = 'Hitchance',
+    Default = 50,
+    Min = 1,
+    Max = 100,
+    Rounding = 0,
+    Compact = false,
+    Callback = function(Value)
+        SilentAim.Config.Hitchance = Value
+    end
+})
+
+SilentAimMainSection:AddDropdown('SilentAimAimpart', {
+    Values = {"Head", "Torso", "Left Leg", "Right Leg", "Left Arm", "Right Arm", "UpperTorso", "LowerTorso", "LeftUpperLeg", "RightUpperLeg", "LeftLowerLeg", "RightLowerLeg", "LeftFoot", "RightFoot", "LeftUpperArm", "RightUpperArm", "LeftLowerArm", "RightLowerArm", "LeftHand", "RightHand"},
+    Multi = true,
+    Text = 'Hitparts',
+    Default = "Head",
+    Callback = function(Value)
+        local T = {}
+
+        for k, v in next, Value do
+            if v then
+                table.insert(T, k)
+            end
+        end
+
+        SilentAim.Config.Hitparts = T
+    end
+})
+
+SilentAimMainSection:AddDropdown('SilentAimPriorityHitpart', {
+    Values = {"Head", "Torso", "Left Leg", "Right Leg", "Left Arm", "Right Arm", "UpperTorso", "LowerTorso", "LeftUpperLeg", "RightUpperLeg", "LeftLowerLeg", "RightLowerLeg", "LeftFoot", "RightFoot", "LeftUpperArm", "RightUpperArm", "LeftLowerArm", "RightLowerArm", "LeftHand", "RightHand"},
+    Multi = false,
+    AllowNull = true,
+    Text = 'Priority Hitpart',
+    Default = nil,
+    Callback = function(Value)
+        SilentAim.Config.PriorityPart = Value
+    end
+})
+
+SilentAimMainSection:AddDropdown('SilentAimMethod', {
+    Values = {"Raycast", "FindPartOnRay", "FindPartOnRayWithIgnoreList", "FindPartOnRayWithWhitelist", "Spherecast"},
+    Multi = false,
+    Text = 'Method',
+    Default = "Raycast",
+    Callback = function(Value)
+        SilentAim.Config.Method = Value
+    end
+})
+
+SilentAimMainSection:AddDivider()
+
+SilentAimMainSection:AddToggle('SilentAimWallCheckEnabled', {
+    Text = "Wall Check",
+    Default = false,
+    Callback = function(Value)
+        SilentAim.Config.WallCheck = Value
+    end
+})
+
+SilentAimMainSection:AddToggle('SilentAimTeamCheckEnabled', {
+    Text = "Team Check",
+    Default = false,
+    Callback = function(Value)
+        SilentAim.Config.TeamCheck = Value
+    end
+})
+
+SilentAimMainSection:AddToggle('SilentAimInvisibleCheckEnabled', {
+    Text = "Invisible Check",
+    Default = false,
+    Callback = function(Value)
+        SilentAim.Config.InvisCheck = Value
+    end
+})
+
+SilentAimMainSection:AddToggle('SilentAimForceFieldCheckEnabled', {
+    Text = "ForceField Check",
+    Default = false,
+    Callback = function(Value)
+        SilentAim.Config.ForceFieldCheck = Value
+    end
+})
+
+SilentAimMainSection:AddDivider()
+
+SilentAimMainSection:AddToggle('SilentAimFOVEnabled', {
+    Text = "FOV Enabled",
+    Default = false,
+    Callback = function(Value)
+        SilentAim.FOVConfig.Enabled = Value
+    end
+})
+
+SilentAimMainSection:AddToggle('SilentAimFOVVisible', {
+    Text = "FOV Visible",
+    Default = false,
+    Callback = function(Value)
+        SilentAim.FOVConfig.Visible = Value
+    end
+})
+
+SilentAimMainSection:AddSlider('SilentAimFOVRadius', {
+    Text = 'Radius',
+    Default = 100,
+    Min = 1,
+    Max = 1000,
+    Rounding = 0,
+    Compact = false,
+    Callback = function(Value)
+        SilentAim.FOVConfig.Radius = Value
+    end
+})
+
+local ESPMainSection = Tabs.Visuals:AddLeftGroupbox('ESP')
+
+ESPMainSection:AddToggle('ESPEnabled', {
+    Text = "Enabled",
+    Default = false,
+    Callback = function(Value)
+        ESP.Config.Enabled = Value
+    end
+})
+
+ESPMainSection:AddToggle('ESPBoxEnabled', {
+    Text = "Box",
+    Default = false,
+    Callback = function(Value)
+        ESP.Config.Box = Value
+    end
+}):AddColorPicker('ESPBoxColor', {
+    Title = 'Box Color',
+    Default = Color3.fromRGB(255, 255, 255),
+    Transparency = nil,
+    Callback = function(Value)
+        ESP.Config.BoxColor = Value
+    end
+})
+
+ESPMainSection:AddToggle('ESPNameEnabled', {
+    Text = "Name",
+    Default = false,
+    Callback = function(Value)
+        ESP.Config.Name = Value
+    end
+}):AddColorPicker('ESPNameColor', {
+    Title = 'Name Color',
+    Default = Color3.fromRGB(255, 255, 255),
+    Transparency = nil,
+    Callback = function(Value)
+        ESP.Config.NameColor = Value
+    end
+})
+
+ESPMainSection:AddToggle('ESPTracersEnabled', {
+    Text = "Tracers",
+    Default = false,
+    Callback = function(Value)
+        ESP.Config.Tracers = Value
+    end
+}):AddColorPicker('ESPTracerColor', {
+    Title = 'Tracer Color',
+    Default = Color3.fromRGB(255, 255, 255),
+    Transparency = nil,
+    Callback = function(Value)
+        ESP.Config.TracerColor = Value
+    end
+})
+
+ESPMainSection:AddToggle('ESPDistanceEnabled', {
+    Text = "Distance",
+    Default = false,
+    Callback = function(Value)
+        ESP.Config.Distance = Value
+    end
+}):AddColorPicker('ESPDistanceColor', {
+    Title = 'Distance Color',
+    Default = Color3.fromRGB(255, 255, 255),
+    Transparency = nil,
+    Callback = function(Value)
+        ESP.Config.DistanceColor = Value
+    end
+})
+
+ESPMainSection:AddToggle('ESPHealthBarEnabled', {
+    Text = "Healthbar",
+    Default = false,
+    Callback = function(Value)
+        ESP.Config.HealthBar = Value
+    end
+}):AddColorPicker('ESPHighHealthColor', {
+    Title = 'High Health Color',
+    Default = Color3.fromRGB(0, 255, 0),
+    Transparency = nil,
+    Callback = function(Value)
+        ESP.Config.HighHealthColor = Value
+    end
+}):AddColorPicker('ESPLowHealthColor', {
+    Title = 'Low Health Color',
+    Default = Color3.fromRGB(255, 0, 0),
+    Transparency = nil,
+    Callback = function(Value)
+        ESP.Config.LowHealthColor = Value
+    end
+})
+
+ESPMainSection:AddToggle('ESPHighlightEnabled', {
+    Text = "Highlight",
+    Default = false,
+    Callback = function(Value)
+        ESP.Config.Chams = Value
+    end
+}):AddColorPicker('ESPHighlightFillColor', {
+    Title = 'Fill Color',
+    Default = Color3.fromRGB(45, 125, 254),
+    Transparency = 0.7,
+    Callback = function(Value, Alpha)
+        ESP.Config.ChamsFillColor = Value
+        ESP.Config.ChamsFillTransparency = Alpha
+    end
+}):AddColorPicker('ESPHighlightOutlineColor', {
+    Title = 'Outline Color',
+    Default = Color3.fromRGB(45, 125, 254),
+    Transparency = 0.5,
+    Callback = function(Value, Alpha)
+        ESP.Config.ChamsOutlineColor = Value
+        ESP.Config.ChamsOutlineTransparency = Alpha
+    end
+})
+
+ESPMainSection:AddDivider()
+
+ESPMainSection:AddToggle('ESPTeamCheckEnabled', {
+    Text = "Team Check",
+    Default = false,
+    Callback = function(Value)
+        ESP.Config.TeamCheck = Value
+    end
+})
+
+ESPMainSection:AddToggle('ESPInvisibleCheckEnabled', {
+    Text = "Invisible Check",
+    Default = false,
+    Callback = function(Value)
+        ESP.Config.InvisibleCheck = Value
+    end
+})
+
+ESPMainSection:AddDivider()
+
+ESPMainSection:AddDropdown('ESPFont', {
+    Values = {"smallest_pixel-7", "ProggyClean"},
+    Multi = false,
+    Text = 'Font',
+    Default = "ProggyClean",
+    Callback = function(Value)
+        getgenv().GlobalFont = Value .. ".font"
+
+        ESP.UpdateFonts()
+    end
+})
+
+local MenuSection = Tabs.Settings:AddLeftGroupbox('Menu')
+
+MenuSection:AddToggle('Keybinds', {
+    Text = "Keybinds",
+    Default = false,
+    Callback = function(Value)
+        Library.KeybindFrame.Visible = Value
+    end
+})
+
+MenuSection:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', {
+    Default = 'RightShift',
+    NoUI = true,
+    Text = 'Menu'
+})
+
+
+Options.MenuKeybind.NoUI = true
+Library.ToggleKeybind = Options.MenuKeybind
+
+SaveManager:SetLibrary(Library)
+SaveManager:SetFolder('FlowTiaPaola/Universal')
+SaveManager:IgnoreThemeSettings()
+SaveManager:BuildConfigSection(Tabs.Settings)
+
+ThemeManager:SetLibrary(Library)
+ThemeManager:SetFolder('FlowTiaPaola/Themes')
+ThemeManager:ApplyToTab(Tabs.Settings)
+ThemeManager:ApplyTheme("BDH")
